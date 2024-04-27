8b1d32f923756e2830a3b27e9c243d01c886d057
dabeng <dabeng413@gmail.com>
2024-04-27 11:00:50 +0800

    populate username

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -13,9 +13,48 @@ export class CommentsService {
   create(createCommentDto: CreateCommentDto): Promise<Comment> {
     return (new this.commentModel(createCommentDto)).save();
   }
+  /*
+*** Reference by other field instead of _id ***
+2024 - Mongoose version 8
 
+suppose you have two schemas:
+
+const playlistSchema = new mongoose.Schema({
+  externalId: String,
+  title: String,
+  videos: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Video',
+  }],
+});
+
+const videoSchema = new mongoose.Schema({
+  externalId: String,
+  title: String,
+});
+And you want to populate the videos field from the playlistSchema based on externalId field from videoSchema:
+
+There is a simple way to achieve this:
+
+  PlaylistModel.find()
+    .populate({
+      path: 'videos',
+      model: 'Video',
+      select: 'title',
+      foreignField: 'externalId',
+    })
+• path: specifies the field in the Announcement schema that you want to populate.
+• model: specifies the model to use for population.
+• select: specifies the fields to select from the referenced document.
+• foreignField: specifies the field in the referenced model (Video in this case) that is used to match with the local field (videos in playlistSchema) for populating references. (instead of the default _id)
+  
+  */
   async findAll(query?: any): Promise<any> {
-    const features = new APIFeatures(this.commentModel.find(), query)
+    const features = new APIFeatures(this.commentModel.find().populate({
+      path: 'author',
+      model: 'User',
+      select: 'username',
+    }), query)
       .filter()
       .sort()
       .select()
