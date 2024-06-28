60c5f975d4276e9c491af19162d78fdcdd20d1a4
dabeng <dabeng413@gmail.com>
2024-06-28 11:36:59 +0800

    add file upload validation

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,11 +1,11 @@
-import { Post, UseInterceptors, UploadedFiles, Controller } from '@nestjs/common';
+import { Post, UseInterceptors, UploadedFiles, Controller, ParseFilePipe, MaxFileSizeValidator, FileTypeValidator } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { FileService } from './file.service';
 
 @Controller('file') // Base route for file-related operations
 export class FileController {
-  constructor(private readonly fileService: FileService) {}
+  constructor(private readonly fileService: FileService) { }
 
   @Post('upload') // Endpoint for uploading files
   @UseInterceptors(
@@ -16,9 +16,14 @@ export class FileController {
           cb(null, file.originalname);
         },
       }),
-    }), 
+    }),
   ) // Intercept and handle file uploads
-  async uploadFile(@UploadedFiles() files: Array<Express.Multer.File>) {
+  async uploadFile(@UploadedFiles(new ParseFilePipe({
+    validators: [
+      new MaxFileSizeValidator({ maxSize: 800 }),
+      new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' }),
+    ],
+  }),) files: Array<Express.Multer.File>) {
     // Delegate file handling to the FileService
     return await this.fileService.uploadFile(files);
   }
